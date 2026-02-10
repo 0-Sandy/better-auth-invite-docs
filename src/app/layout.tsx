4426bd37097c2f91dfbc3c492def0424e59e66f0
0-Sandy <github@sandy00.aleeas.com>
2026-02-10 18:15:46 -0300

    chore: add google search console token

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,13 @@
+import type { Metadata } from 'next';
 import { RootProvider } from "fumadocs-ui/provider/next";
 import "./global.css";
 import { Inter } from "next/font/google";
+
+export const metadata: Metadata = {
+  verification: {
+    google: "2_Rk36ZAi2agX8jVuzvGnG_mgC_4NSyeeQlTfyXoIdA"
+  }
+};
 
 const inter = Inter({
   subsets: ["latin"],
