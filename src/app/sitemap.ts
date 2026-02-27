ee359b3975895148fe90255d10e976db00b40499
0-Sandy <github@sandy00.aleeas.com>
2026-02-27 15:55:33 -0300

    fix: add blogs to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,25 +1,35 @@
 import type { MetadataRoute } from "next";
 import { baseUrl } from "@/lib/metadata";
-import { source } from "@/lib/source";
+import { blog, source } from "@/lib/source";
 
 export const revalidate = false;
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const url = (path: string): string => new URL(path, baseUrl).toString();
-  const items = await Promise.all(
-    source.getPages().map(async (page) => {
-      if (page.data.type === "openapi") return;
+  // biome-ignore lint/suspicious/useIterableCallbackReturn: filtered out in the end
+  const docPages = source.getPages().map((page) => {
+    if (page.data.type === "openapi") return;
 
-      const lastModified = page.data.lastModified;
+    const lastModified = page.data.lastModified;
 
-      return {
-        url: url(page.url),
-        lastModified: lastModified ? new Date(lastModified) : undefined,
-        changeFrequency: "weekly",
-        priority: 0.5,
-      } satisfies MetadataRoute.Sitemap[number];
-    }),
-  );
+    return {
+      url: url(page.url),
+      lastModified: lastModified ? new Date(lastModified) : undefined,
+      changeFrequency: "weekly",
+      priority: 0.5,
+    } satisfies MetadataRoute.Sitemap[number];
+  });
+
+  const blogPages = blog.getPages().map((page) => {
+    const lastModified = page.data.lastModified;
+
+    return {
+      url: url(page.url),
+      lastModified: lastModified ? new Date(lastModified) : undefined,
+      changeFrequency: "weekly",
+      priority: 0.5,
+    } satisfies MetadataRoute.Sitemap[number];
+  });
 
   return [
     {
@@ -27,11 +37,17 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       changeFrequency: "monthly",
       priority: 1,
     },
+    {
+      url: url("/blog"),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
     /*{
       url: url("/showcase"),
       changeFrequency: "monthly",
       priority: 0.8,
     },*/
-    ...items.filter((v) => v !== undefined),
+    ...docPages.filter((v) => v !== undefined),
+    ...blogPages,
   ];
 }
